2cab8b1da3787cedb8dbdbf891835c5e68c94537
IxtliTakeru <ixtligiovin@gmail.com>
2023-08-15 10:27:53 +0800

    [Todo Layout] implement todo card layout and delete and update important function

--- todo-list/src/components/TodoList.vue
+++ todo-list/src/components/TodoList.vue
@@ -1,49 +1,178 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useTodoListStore } from '../stores/todolist.js'
+import { EllipsisVerticalIcon, StarIcon } from '@heroicons/vue/24/outline'
+import { StarIcon as SolidStarIcon} from '@heroicons/vue/24/solid'
 
+// get data/function by pinia
 const todoListStore = useTodoListStore()
-const {todoList, deleteTodo, updateTodo} = storeToRefs(todoListStore)
-// let id = 0
-// const newTodo = ref('')
-// const todos = ref([
-//     { id: id++, text: 'Learn HTML' },
-//     { id: id++, text: 'Learn JavaScript' },
-//     { id: id++, text: 'Learn Vue' }
-// ])
+// for data
+const { todoList } = storeToRefs(todoListStore)
+// for function
+const { deleteTodo, updateTodo, updateImpotant } = todoListStore
+console.log("todoList.length", todoList.value.length)
+console.log("todoList", todoList.value)
 
-function addTodo() {
-    // todos.value.push({ id: id++, text: newTodo.value })
-    // newTodo.value = ''
-}
+// calculate time period from create date to now and convert it to readable wording
+const period = computed(() => (createdDate) => {
+    const now = Date.now()
+    console.log("now", now)
+    console.log("createdDate", createdDate)
 
-function removeTodo(todo) {
-    // todos.value = todos.value.filter((t) => t !== todo)
+    var seconds = Math.floor((now - createdDate) / 1000);
+
+    var interval = seconds / 31536000;
+    if (interval > 1) {
+        return Math.floor(interval) + " years ago";
+    }
+
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return Math.floor(interval) + " months ago";
+    }
+
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return Math.floor(interval) + " days ago";
+    }
+
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return Math.floor(interval) + " hours ago";
+    }
+
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+})
+
+// function updateTodo() {
+// todos.value.push({ id: id++, text: newTodo.value })
+// newTodo.value = ''
+// }
+
+function removeTodo(index) {
+    console.log("removeTodo", index)
+    deleteTodo(index)
 }
 
 </script>
 
 <template>
     <div class="app-todolist">
-        <form @submit.prevent="addTodo">
-            <input v-model="newTodo">
-            <button>Add Todo</button>
-        </form>
-        <ul>
-            <li v-for="todo in todoList" :key="todo.uuid">
-                {{ todo.title }}
-                <button @click="removeTodo(todo)">X</button>
-            </li>
-        </ul>
+        <div v-for="(todo, index) in todoList" :key="todo.uuid" class="todo-cardview">
+            <div class="todo-cardview-header">
+                <p class="todo-cardview-title">{{ todo.title }}</p>
+                <p class="todo-cardview-content">{{ todo.content }}</p>
+            </div>
+            <!-- display staricon not checkbox -->
+            <div class="todo-cardview-footer">
+                <div>
+                    <p class="todo-cardview-period"> {{ period(todo.date) }} </p>
+                </div>
+                <div>
+                    <!-- Todo: should find the solution by checkbox, use a workaround(v-if, v-else) now -->
+                    <SolidStarIcon v-if="todo.isImportant" class="star-icon" @click="updateImpotant(index)"/>
+                    <StarIcon v-else class="star-icon" @click="updateImpotant(index)"/>
+                    <EllipsisVerticalIcon class="more-function-icon" @click="removeTodo(index)" />
+                </div>
+            </div>
+
+        </div>
+
+
     </div>
 </template>
 
 <style scoped>
 .app-todolist {
     height: 100%;
-    background-color: goldenrod;
-    margin: 10px;
-    border-radius: 32px;
+    margin-top: 20px;
+    margin-left: 40px;
+    margin-right: 40px;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 220px);
+}
+
+.todo-cardview {
+    width: 200px;
+    height: 200px;
+    border-radius: 16px;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.todo-cardview-header {
+    margin: 20px;
+}
+
+.todo-cardview-footer {
+    margin: 20px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.todo-cardview-title {
+    width: 160px;
+    color: black;
+    size: 20px;
+    font-weight: bold;
+    word-wrap: break-word;
+}
+
+.todo-cardview-content {
+    color: black;
+    width: 160px;
+    size: 14px;
+    word-wrap: break-word;
+}
+
+.todo-cardview-period {
+    color: blueviolet;
+    font-size: 10px;
+}
+
+.checkbox-container {
+    display: flex;
+    flex-direction: column;
+}
+
+.star-checkbox input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+.star-checkbox label {
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+    stroke: black;
+}
+
+.star-checkbox input:checked+label svg {
+    fill: gold;
+    transition: 0.2s;
+}
+
+.star-icon {
+    width: 24px;
+    height: 24px;
+    color: gold;
+}
+
+.more-function-icon {
+    width: 24px;
+    height: 24px;
+    color: black;
+}
+.more-function-icon:hover,
+.more-function-icon:checked {
+    color: gray;
 }
 </style>
--- todo-list/src/stores/todolist.js
+++ todo-list/src/stores/todolist.js
@@ -11,16 +11,22 @@ export const useTodoListStore = defineStore('todolist', () => {
   //      isComplete: boolean, 
   //      isImportant: boolean} ]
 
-  const todoList = ref([{}])
+  const todoList = ref([])
 
   //   const sortBy = computed(() => {})
+
+  // calculate time perios from now to create date and format it to "{min} mins ago".
+  // const timePeriod = computed((createdDate) => {
+  //   const now = Date.now()
+  //   return now - createdDate
+  // })
 
   function addTodo(todo) {
     console.log("todo", todo)
     todoList.value.push({
       uuid: todoList.value.length,
       title: todo.title,
-      date: todo.date,
+      date: Date.now(),
       content: todo.content,
       isComplete: todo.isComplete,
       isImportant: todo.isImportant
@@ -33,9 +39,17 @@ export const useTodoListStore = defineStore('todolist', () => {
     todoList.value.splice(index, 1);
   }
 
-  function updateTodo() {
-
+  function updateTodo(index, updateTodo) {
+    const updateTarget = todoList.value.at(index)
+    console.log("updateTarget", updateTarget)
   }
 
-  return { todoList, addTodo, deleteTodo, updateTodo }
+  function updateImpotant(index) {
+    console.log("updateImpotant", index)
+    const updateTarget = todoList.value.at(index)
+    console.log("updateImpotant", updateTarget)
+    updateTarget.isImportant = !updateTarget.isImportant
+  }
+
+  return { todoList, addTodo, deleteTodo, updateTodo, updateImpotant }
 })
