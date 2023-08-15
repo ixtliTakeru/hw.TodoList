a972500c5dba1b4aab6af02626406b359624717d
IxtliTakeru <ixtligiovin@gmail.com>
2023-08-15 10:25:50 +0800

    [Header Layout] refactor and format css naming and clear input date after new todo added

--- todo-list/src/components/Header.vue
+++ todo-list/src/components/Header.vue
@@ -1,14 +1,10 @@
 <script setup>
 import { computed, ref } from 'vue'
 import 'element-plus/es/components/message/style/css'
-import { ElMessage } from 'element-plus'
 import { useTodoListStore } from "../stores/todolist.js"
 
 // TodoListStore
 const todoListStore = useTodoListStore()
-const newId = computed (() => {
-    return todoListStore.todoList.values.length + 1
-})
 const addTodoEvent = (todo) => {
     todoListStore.addTodo(todo)
 }
@@ -18,35 +14,46 @@ const dialogVisible = ref(false)
 
 // new todo
 const tempTodo = ref({
-    uuid: 0,
-    title: "hi",
-    date: 123,
-    content: "Hello World",
-    isComplete: Boolean(Math.round(Math.random())),
-    isImportant: true
+    title: "",
+    content: "",
+    isComplete: false,
+    isImportant: false
 })
+
+// TODO: should create a new temp tode when dialog show
+function resetTempTodo() {
+    tempTodo.value.title = "";
+    tempTodo.value.content = "";
+    tempTodo.value.isComplete = false;
+    tempTodo.value.isImportant = false;
+}
+
+// TODO: shuld check title and content are empty or not before add 
+function checkInputContent() {
+}
+
 </script>
 
 <template>
     <div class="app-header">
         <div>
             <span class="project">Project</span>
-            <span class="projectName">Todo List</span>
+            <span class="project-name">Todo List</span>
         </div>
 
-        <el-button text @click="dialogVisible = true">
+        <el-button @click="dialogVisible = true" class="add-todo-btn">
             Add a new todo
         </el-button>
 
         <el-dialog v-model="dialogVisible" title="Add a new todo" width="30%">
             <el-form>
-                <el-form-item label="Title" :label-width="formLabelWidth">
+                <el-form-item label="Title" >
                     <el-input v-model="tempTodo.title"></el-input>
                 </el-form-item>
-                <el-form-item label="Content" :label-width="formLabelWidth">
+                <el-form-item label="Content" >
                     <el-input type="textarea" :rows="4" v-model="tempTodo.content"></el-input>
                 </el-form-item>
-                <el-form-item label="Star" :label-width="formLabelWidth">
+                <el-form-item label="Star" >
                     <el-checkbox v-model="tempTodo.isImportant">is important</el-checkbox>
                 </el-form-item>
 
@@ -57,7 +64,7 @@ const tempTodo = ref({
                     <el-button @click="dialogVisible = false">
                         Cancel
                     </el-button>
-                    <el-button type="primary" @click="dialogVisible = false, addTodoEvent(tempTodo)">
+                    <el-button type="primary" @click="dialogVisible = false, addTodoEvent(tempTodo), resetTempTodo()">
                         Add
                     </el-button>
                 </span>
@@ -75,7 +82,6 @@ const tempTodo = ref({
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    align-items: center;
 }
 
 .project {
@@ -87,7 +93,7 @@ const tempTodo = ref({
     background-color: transparent;
 }
 
-.projectName {
+.project-name {
     font-size: 30px;
     line-height: 32px;
     font-weight: 500;
@@ -95,16 +101,24 @@ const tempTodo = ref({
     background-color: transparent;
 }
 
-.addTodoBtn {
-    font-size: 12px;
+.add-todo-btn {
+    height: 40px;
+    width: 160px;
+    font-size: 18px;
     color: white;
-    background-color: rgba(143, 201, 237, 0.7);
+    background-color: rgba(85, 182, 243, 0.7);
     box-shadow: none;
     border: none;
-    border-radius: 20px;
+    border-radius: 24px;
     margin-right: 10px;
     padding: 10px;
-    font-weight: 700;
+}
+
+.add-todo-btn:focus,
+.add-todo-btn:hover {
+    background-color: rgb(57, 149, 180, 0.7);
+    box-shadow: none;
+    border: none;
 }
 
 .dialog-footer button:first-child {
