3c081c1e62fd8320aa01199b1ffcfdd887f23a8f
IxtliTakeru <ixtligiovin@gmail.com>
2023-08-15 10:24:02 +0800

    add heroicon libs and use icons in the sidebar layout

--- todo-list/src/components/SideBar.vue
+++ todo-list/src/components/SideBar.vue
@@ -1,57 +1,27 @@
 <script setup>
+import { TrashIcon, ArrowTrendingUpIcon, CurrencyDollarIcon, UserIcon, CalendarIcon, RectangleGroupIcon, ArchiveBoxIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
     <div class="app-sidebar">
         <div>
-            <img src = "../assets/logo.svg" alt="My Happy SVG"/>
+            <img src = "../assets/logo.svg" alt="" style="margin-bottom: 20px;"/>
 
-            <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                stroke-linecap="round" stroke-linejoin="round">
-                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                <polyline points="9 22 9 12 15 12 15 22"></polyline>
-            </svg>
+            <RectangleGroupIcon class="icon"/>
 
-            <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"
-                stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
-                <defs></defs>
-                <path d="M21.21 15.89A10 10 0 118 2.83M22 12A10 10 0 0012 2v10z"></path>
-            </svg>
+            <ArrowTrendingUpIcon class="icon"/>
 
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                stroke-linecap="round" stroke-linejoin="round" class="icon">
-                <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                <line x1="16" y1="2" x2="16" y2="6"></line>
-                <line x1="8" y1="2" x2="8" y2="6"></line>
-                <line x1="3" y1="10" x2="21" y2="10"></line>
-            </svg>
+            <CalendarIcon class="icon"/>
 
-            <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"
-                stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
-                <defs></defs>
-                <circle cx="12" cy="12" r="3"></circle>
-                <path
-                    d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z">
-                </path>
-            </svg>
+            <UserIcon  class="icon"/>
+
+            <CurrencyDollarIcon  class="icon"/>
         </div>
 
         <div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                stroke-linecap="round" stroke-linejoin="round" class="icon">
-                <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                <line x1="16" y1="2" x2="16" y2="6"></line>
-                <line x1="8" y1="2" x2="8" y2="6"></line>
-                <line x1="3" y1="10" x2="21" y2="10"></line>
-            </svg>
+            <ArchiveBoxIcon class="icon"/>
 
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                stroke-linecap="round" stroke-linejoin="round" class="icon">
-                <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                <line x1="16" y1="2" x2="16" y2="6"></line>
-                <line x1="8" y1="2" x2="8" y2="6"></line>
-                <line x1="3" y1="10" x2="21" y2="10"></line>
-            </svg>
+            <TrashIcon class="icon"/>
         </div>
 
     </div>
@@ -64,9 +34,7 @@
     padding: 20px 16px;
     display: flex;
     flex-direction: column;
-    align-items: center;
     justify-content: space-between;
-    align-items: center;
 }
 
 .icon {
@@ -80,5 +48,13 @@
     margin-top: 15px;
     margin-bottom: 15px;
     margin-left: 6px;
+}
+.icon:hover,
+.icon:checked {
+    stroke: white;
+    background-color: black;
+    border-radius: 50%;
+    object-fit: cover;
+    padding: 5px;
 }
 </style>
