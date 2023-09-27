ec8a94d194a6d0796428cec90dd6f8476860c585
IxtliTakeru <ixtligiovin@gmail.com>
2023-09-27 10:23:39 +0800

    add libs "Tailwind Css"

---
+++ todo-list/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /** 原本 2.0 的 purge 在 3.0 改成 content */
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
